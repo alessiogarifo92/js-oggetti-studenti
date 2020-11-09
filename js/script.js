b6fc4de413476b1f18f22685946d064eff6d5caf
alessiogarifo92 <alessiogarifo92@gmail.com>
2020-11-09 16:33:19 +0100

    punto 3 ex completato

--- js/script.js
+++ js/script.js
@@ -29,34 +29,66 @@ for (var key in studente) {
 var studenti = [
   {
     'nome' : 'Alessio',
-    'cognome' : 'Garifo'
+    'cognome' : 'Garifo',
+    'eta' : 10
   },
 
   {
     'nome' : 'Marco',
-    'cognome' : 'Salvatore'
+    'cognome' : 'Salvatore',
+    'eta' : 10
   },
 
   {
     'nome' : 'Martina',
-    'cognome' : 'Polimeno'
+    'cognome' : 'Polimeno',
+    'eta' : 10
   },
 
   {
     'nome' : 'Soraya',
-    'cognome' : 'Saccenti'
+    'cognome' : 'Saccenti',
+    'eta' : 10
   },
 
   {
     'nome' : 'Alberto',
-    'cognome' : 'Vannacci'
+    'cognome' : 'Vannacci',
+    'eta' : 10
   }
 ];
 console.log(studenti);
 // ciclo ogni studente e stampo nome e cognome
+// primo modo
 for (var i = 0; i < studenti.length; i++) {
-  for (var key in studenti[i]) {
 
-  }
-  console.log(studenti[i]);
-}
+  console.log(studenti[i].nome + ' ' + studenti[i].cognome);
+};
+
+// secondo modo e entrambi danno stesso risultato
+// for (var key in studenti) {
+//   console.log(studenti[key].nome + ' ' + studenti[key].cognome);
+// }
+
+/////////////// PUNTO TRE////////////
+
+// chiediamo ad utente di inserire con prompt nuovo oggetto con nome cognome e eta
+
+var nomeUtente = prompt('Inserisci il tuo nome');
+
+var cognomeUtente = prompt('Inserisci il tuo cognome');
+
+var etaUtente = parseInt(prompt('Inserisci la tua età'));
+
+// inseriamo nuovo oggetto studente da dati inseriti in prompt
+// creiamo nuova variabile nuovoStudente per nuovo oggetto
+var nuovoStudente = {};
+
+// andiamo a dare ad ogni elemento inserito nei prompt la rispettiva proprieta all interno dell oggetto
+nuovoStudente.nome = nomeUtente;
+nuovoStudente.cognome = cognomeUtente;
+nuovoStudente.eta = etaUtente;
+
+// pushiamo nell array il nuovo oggetto
+studenti.push(nuovoStudente);
+console.log(studenti[i].nome + ' ' + studenti[i].cognome + ' ' + studenti[i].eta);
